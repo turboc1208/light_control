32cfca1020d7f7ccdeb0dc7ac8f3572df52d0c16
turboc1208 <chip.cox@coxdogs.com>
2017-03-14 12:31:03 -0500

    updated lights because of swapping out dimmers for standard switches

--- light_control.py
+++ light_control.py
@@ -5,23 +5,76 @@ class light_control(appapi.my_appapi):
 
   def initialize(self):
     self.log("lightcontrol")
-   
+    self.dim_rate=50   
     # Control_dict structure
     #"device:{ light : { "type": dimmer, ondevicestate: ondimmerstate, offdevicestate: "0"},
     #        { light : { "type": switch, "on": onswitchstate, "off": "off"}}
 
     self.control_dict={"light.den_fan_light":{"light.den_fan_light":{"type":"dimmer","on":"50","off":"0","last_brightness":""}},
+                       "light.den_fan":{"light.den_fan":{"type":"dimmer","on":"50","off":"0","last_brightness":""}},
                        "switch.breakfast_room_light":{"switch.breakfast_room_light":{"type":"switch","on":"on","off":"off"}},
                        "media_player.dentv":{"light.den_fan_light":{"type":"dimmer","on":"10","off":"last","last_brightness":""},
-                                             "switch.breakfast_room_light":{"type":"switch","on":"off","off":"on"}},
-                       "light.outdoor_patio_light":{"light.outdoor_patio_light":{"type":"dimmer","on":"255","off":"0","last_brightness":""}},
+                                             "switch.breakfast_room_light":{"type":"switch","on":"off","off":"last"}},
+                       "switch.downstairs_hallway_light":{"switch.downstairs_hallway_light":{"type":"switch","on":"on","off":"off"}},
                        "light.master_light_switch":{"light.master_light_switch":{"type":"dimmer","on":"255","off":"0","last_brightness":""}} }
 
     for control in self.control_dict:
       self.listen_state(self.state_change,control,attributes="all")
 
+    self.check_current_state()
+
+  def check_current_state(self):
+    newstate={}
+    for light in self.control_dict:
+      dev,entity=self.split_entity(light)
+      if dev in ["light","switch"]:
+        dev_state=self.get_state(light)
+        for sublight in self.control_dict[light]:
+          newstate[sublight]=self.control_dict[light][sublight][dev_state]
+
+    self.log("after first pass newstate={}".format(newstate))
+
+    for light in self.control_dict:
+      dev,entity=self.split_entity(light)
+      if dev not in ["light","switch"]:
+        dev_state=self.get_state(light)
+        for sublight in self.control_dict[light]:
+          newstate[sublight]=self.control_dict[light][sublight][dev_state]
+    self.log("after second pass newstate={}".format(newstate))
+
+    for light in newstate:
+      if newstate[light]=="on":
+        self.log("Turning {} on".format(light))
+        self.turn_on(light)
+      elif newstate[light]=="off":
+        self.log("Turning {} off".format(light))
+        self.turn_off(light)
+      elif newstate[light]=="dim":
+        self.log("Dimming {}".format(light))
+        self.run_in(self.dim_light,600,light=light)                                       # check again in 10 minutes to start diming again if spot is done
+      elif newstate[light]=="last":
+        self.log("dont know last value for {} leaving it alone".format(light))
+        continue
+      else:
+        try:
+          dimamt=int(newstate[light])
+        except ValueError:
+          self.log("invalid new light state {}".format(newstate[light]))
+          continue
+        except:
+          raise
+
+        if dimamt==0:
+          self.log("turning off dimmer {}".format(light))
+          self.turn_off(light)
+        else:
+          self.log("setting {} to {} brightness".format(light,dimamt))
+          self.turn_on(light,brightness=dimamt)
+
+
   def state_change(self,entity,attributes,old,new,kwargs):
-    if not old==new:     # control entity changed state
+    self.log("old = {} new= {}".format(old,new))
+    if not ((old==new) or (new=="unavailable")):     # control entity changed state
       for light in self.control_dict[entity]:            # loop through lights controled by control entity
         self.set_light_state(entity,light,new)
 
@@ -43,22 +96,52 @@ class light_control(appapi.my_appapi):
           self.turn_off(light)
       else:
         self.log("light is off leave it that way")
+    elif self.control_dict[entity][light][entity_state]=="dim":      # dim light till it's off
+      self.log("dim light {} till it's off".format(light))
+      if self.get_state(light,attribute="state")=="on":              # check if the light is on
+        self.turn_on(light,brightness=255)
+        self.run_in(self.dim_light,60,light=light)                 # schedule a call back to dim the light some
     else:
-      self.log("New desired state is on or has a dimmmer value {}".format(self.control_dict[entity][light][entity_state]))
+      self.log("New desired state for {} is on or has a dimmmer value {}".format(light,self.control_dict[entity][light][entity_state]))
       if self.get_state(light,attribute="state")=="on":
-        self.log("current state is on")
-        if self.control_dict[entity][light]["type"]=="dimmer":
-          self.log("light is a currently on dimmer seting value to {} ".format(self.control_dict[entity][light][entity_state]))
+        self.log("{}'s current state is on".format(light))                            # light is already on
+        if self.control_dict[entity][light]["type"]=="dimmer":     # light is a dimmer so save current state and adjust to new value
+          self.log("{} is a currently on dimmer seting value to {} ".format(light,self.control_dict[entity][light][entity_state]))
           self.control_dict[entity][light]["last_brightness"]=self.get_state(light,attribute="brightness")
-          self.turn_on(light,brightness=self.control_dict[entity][light][entity_state])
+          if self.control_dict[entity][light][entity_state]=="dim":
+            self.turn_on(light,brightness=255)
+          else:
+            self.turn_on(light,brightness=self.control_dict[entity][light][entity_state])
         else:
-          self.log("Light is a currently on switch")
+          self.log("{} is a currently on switch".format(light))
           self.turn_on(light)
-      else:
+      else:                                                        # light is off
         if self.control_dict[entity][light]["type"]=="dimmer":
-          self.log("light is a currently off Dimmer")
+          self.log("{} is a currently off Dimmer".format(light))
           self.turn_off(light)
         else:
-          self.log("light is a currently off switch")
+          self.log("{} is a currently off switch".format(light))
           self.turn_off(light)
 
+  def dim_light(self,kwargs):
+    light=kwargs.get("light",None)
+    self.log("dimming light={}".format(light))
+    if self.get_state(light)=="on":
+      devtypes=["light","switch"]
+      spot_lights=self.build_entity_list("group.app_spotcontrol_spots_lights",devtypes)
+      if not ((self.get_state("input_boolean.spot")=="on") and (light in spot_lights)):   # are we dealing with an exception (spot cleaning)
+        current_brightness=self.get_state(light,"brightness")
+        if current_brightness>=self.dim_rate:                                         # not an exception, is the light still bright enough to see
+          self.turn_on(light,brightness= current_brightness-self.dim_rate) # yes, so dim it somemore
+          self.log("new brightness={}".format(self.get_state(light,"brightness")))
+          self.run_in(self.dim_light,300,light=light)                                      # schedule to run again
+        else:
+          self.turn_off(light)                                                            # to dim to see so turn it off
+      else:                                                                               # else this is an exception (spot cleaning)
+        self.log("exception spot is cleaning")
+        self.turn_on(light,brightness=255)
+        self.log("returning to full brightness")
+        self.run_in(self.dim_light,600,light=light)                                       # check again in 10 minutes to start diming again if spot is done
+    else:
+      self.log("light is finally off don't schedule anymore checks")
+    
